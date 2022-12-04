ae41810c8e02f2d274bff1b6a63c8b768ccc61cf
Muhammed Said ÖĞÜT <msaid.1996@hotmail.com>
2022-12-04 21:06:47 +0300

    O(n^2) updated with O(n)

--- Return_Duplicates_(Not_Leetcode).java
+++ Return_Duplicates_(Not_Leetcode).java
@@ -2,16 +2,20 @@ public class Solution {
 public String returnDuplicates(String my_input){
 
         List<Character> characterList = new ArrayList<>();
+        List<Character> duplicateList = new ArrayList<>();
         for (int i=0; i < my_input.length(); i++){
-            for (int j =i+1; j < my_input.length(); j++){
-                if (my_input.charAt(i) == my_input.charAt(j) && !characterList.contains(my_input.charAt(i))){
-                    characterList.add(my_input.charAt(i));
+            if(!characterList.contains(my_input.charAt(i))){
+                characterList.add(my_input.charAt(i));
+            }
+            else {
+                if (!duplicateList.contains(my_input.charAt(i))){
+                    duplicateList.add(my_input.charAt(i));
                 }
             }
         }
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (Character character: characterList){
+        for (Character character: duplicateList){
             stringBuilder.append(character);
         }
 
