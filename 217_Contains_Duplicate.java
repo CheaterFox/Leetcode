0a52baf4e2c718742886564c3268b3dd49af4408
Muhammed Said ÖĞÜT <msaid.1996@hotmail.com>
2022-12-29 18:20:14 +0300

    217 added

---
+++ 217_Contains_Duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> set = new HashSet<Integer>();
+
+		for(int i : nums) {
+
+			if(!set.add(i)) {
+				return true;
+            }
+        } 
+
+		return false;
+    }
+}
